2ad3456901b63aaf57374b356bc86399f5dc2d3c
George <giyawe98@gmail.com>
2020-06-04 22:12:13 +0200

    Basic Register Form implementiert. Kann aber noch nicht einsehen ob daten geliefert werden.

---
+++ pits-webserver-auth/src/views/Register.vue
@@ -0,0 +1,18 @@
+<template>
+     <div>
+      <div>
+        <h2>Name</h2>
+        <input type="text" v-model="username" />
+      </div>
+
+      <div>
+        <h2>Password</h2>
+        <input type="password" v-model="password" />
+      </div>
+
+      <div>
+        <h2>Submit</h2>
+        <input type="submit" value="Submit" v-on:click="submit" />
+      </div>
+    </div>
+</template>
