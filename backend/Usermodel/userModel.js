108de3721646e36a7c0b4e991daa25ddfedb15b4
n-pola <n.polarek@live.de>
2020-06-06 21:12:38 +0200

    add expire to JWT token

--- backend/Usermodel/userModel.js
+++ backend/Usermodel/userModel.js
@@ -4,24 +4,28 @@ const jwt = require('jsonwebtoken');
 const Usermodel = new mongoose.Schema({
   userName: {
     type: String,
-    required: true,
+    required: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
   },
   currentOtp: {
     type: Number,
-    required: true,
+    required: true
   },
   otpCount: {
     type: Number,
-    required: true,
-  },
+    required: true
+  }
 });
 
 Usermodel.methods.generateAuthToken = function () {
-  const token = jwt.sign({ _id: this._id, name: this.userName }, process.env.JWT_SECRET_KEY);
+  const token = jwt.sign(
+    { _id: this._id, name: this.userName },
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: '2d' }
+  );
   return token;
 };
 
