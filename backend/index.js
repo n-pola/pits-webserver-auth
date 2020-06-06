29a19315b64b685d37b80b82f58c70bb1c94c7ed
n-pola <n.polarek@live.de>
2020-06-06 18:49:13 +0200

    create frontend/backend folders and sort files

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const routes = require('./routes');
+const Mongoose = require('mongoose');
+const app = express();
+const helmet = require('helmet');
+
+app.use(helmet());
+app.use(express.json());
+app.use(routes);
+app.listen(3000, () => {
+  console.log('Server running');
+});
+
+Mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch((error) => console.log(JSON.stringify(error)));
+const db = Mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('connected to database'));
---
+++ backend/routes.js
@@ -0,0 +1,102 @@
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const getToken = require('./middlewares/getToken');
+const User = require('./Usermodel/userModel');
+const bcrypt = require('bcrypt');
+
+router.post('/register', async (req, res) => {
+  try {
+    let hash = bcrypt.hash(req.body.password, 14);
+    const toRegister = new User({
+      userName: req.body.userName,
+      password: await hash
+    });
+
+    console.log(toRegister);
+
+    jwt.sign({ toRegister }, process.env.JWT_SECRET_KEY, async (err, token) => {
+      await toRegister.save((err) => {
+        if (err) {
+          let err = 'Upsie Whoopise something went wrong while saving.';
+          res.Status(500);
+          res.json(err);
+        } else {
+          res.json(toRegister);
+        }
+      });
+    });
+  } catch (error) {
+    res.send({ error });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  let loginUser = {
+    userName: req.body.username,
+    password: req.body.password
+  };
+
+  await User.findOne({ userName: req.body.userName }, (err, User) => {
+    if (err || !User || !bcrypt.compareSync(req.body.password, User.password)) {
+      res.status(403);
+      res.json('You shouldnt be here.');
+    } else {
+      jwt.sign(
+        { loginUser },
+        process.env.JWT_SECRET_KEY,
+        { expiresIn: '180s' },
+        async (err, token) => {
+          res.status(200);
+          res.json(`Welcome  ${User.userName} with the key ${token}`);
+        }
+      );
+    }
+  });
+});
+
+router.get('/seminare', getToken, async (req, res) => {
+  await jwt.verify(
+    req.token,
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: '10s' },
+    (err, authData) => {
+      if (err || req.token == undefined) {
+        res.sendStatus(401);
+      } else {
+        res.json(authData);
+      }
+    }
+  );
+});
+
+router.post('/seminare/:id', getToken, async (req, res) => {
+  await jwt.verify(
+    req.token,
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: '10s' },
+    (err, authData) => {
+      if (err || req.token == undefined) {
+        res.sendStatus(401);
+      } else {
+        res.json(authData);
+      }
+    }
+  );
+});
+
+router.post('/settings/otp', getToken, async (req, res) => {
+  await jwt.verify(
+    req.token,
+    process.env.JWT_SECRET_KEY,
+    { expiresIn: '10s' },
+    (err, authData) => {
+      if (err || req.token == undefined) {
+        res.sendStatus(401);
+      } else {
+        res.json(authData);
+      }
+    }
+  );
+});
+
+module.exports = router;
