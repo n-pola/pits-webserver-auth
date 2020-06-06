30114a2f42fcc262f15e5b68dc1c11b61181373c
n-pola <n.polarek@live.de>
2020-06-06 20:46:56 +0200

    Update Usermodel to save OTP

--- backend/Usermodel/userModel.js
+++ backend/Usermodel/userModel.js
@@ -1,15 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 
 const Usermodel = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  currentOtp: {
+    type: Number,
+    required: true,
+  },
+  otpCount: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('User', Usermodel)
+module.exports = mongoose.model('User', Usermodel);
