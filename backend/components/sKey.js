dcc997c021f6882f390293ca6bd66755cc353c26
n-pola <n.polarek@live.de>
2020-06-06 21:05:09 +0200

    add basic s/key

---
+++ backend/components/sKey.js
@@ -0,0 +1,26 @@
+const otp = require('otplib');
+
+const { hotp } = otp;
+
+function verifyOTP(savedOTP, newOTP, counter) {
+  const isValid = hotp.check(savedOTP, newOTP, counter);
+  console.log(isValid);
+}
+
+async function generateOTP(userName, hashedPW) {
+  const pwList = [];
+  const current_date = new Date();
+  const secret = `${
+    userName + hashedPW + current_date.getMilliseconds() + Math.random() * 10
+  }sgsadrgsgsa987dr6gy87fg`;
+  pwList[0] = hotp.generate(secret, 1200);
+  console.log(secret);
+  for (let i = 0; i < 49; i++) {
+    const newEntry = await hotp.generate(pwList[i], i + 1);
+    pwList.push(newEntry);
+  }
+  console.log(pwList);
+  return pwList;
+}
+
+module.exports = { generateOTP, verifyOTP };
