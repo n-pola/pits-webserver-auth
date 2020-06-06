f294f7a687475954b8e8a9a8bd606e5fb346838f
n-pola <n.polarek@live.de>
2020-06-06 22:25:07 +0200

    return State of verifyOTP

--- backend/components/sKey.js
+++ backend/components/sKey.js
@@ -4,7 +4,8 @@ const { hotp } = otp;
 
 function verifyOTP(savedOTP, newOTP, counter) {
   const isValid = hotp.check(savedOTP, newOTP, counter);
-  console.log(isValid);
+  if (isValid) return true;
+  return false;
 }
 
 async function generateOTP(userName, hashedPW) {
@@ -13,7 +14,7 @@ async function generateOTP(userName, hashedPW) {
   const secret = `${
     userName + hashedPW + current_date.getMilliseconds() + Math.random() * 10
   }sgsadrgsgsa987dr6gy87fg`;
-  pwList[0] = hotp.generate(secret, 1200);
+  pwList[0] = hotp.generate(secret, 0);
   console.log(secret);
   for (let i = 0; i < 49; i++) {
     const newEntry = await hotp.generate(pwList[i], i + 1);
