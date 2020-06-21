f3e461df2a26c96a7883f20a3c59eab4ccb9cbeb
n-pola <n.polarek@live.de>
2020-06-21 23:15:51 +0200

    add Action and mutations to book Seminar

--- frontend/src/store/seminars.js
+++ frontend/src/store/seminars.js
@@ -1,17 +1,37 @@
 import { userService } from "../services";
 
 const state = {
-  all: {}
+  all: {},
+  booking: {}
 };
 
 const actions = {
-  getAll({ commit }) {
+  getAll({ dispatch, commit }) {
     commit("getAllRequest");
 
     userService.getAll().then(
       seminars => commit("getAllSuccess", seminars),
-      error => commit("getAllFailure", error)
+      error => {
+        commit("getAllFailure", error);
+        dispatch("alert/error", error, { root: true });
+      }
     );
+  },
+  bookSeminar({dispatch, commit}, {user, id, token}) {
+    commit("bookSeminarRequest")
+    userService.bookSeminar(id, token).then(
+      seminar => {
+        commit("bookSeminarSuccess");
+        dispatch("account/addSeminar", {user, seminar}, {root: true});
+        dispatch("alert/success", seminar.message, { root: true });
+      },
+      error => {
+        commit("bookSeminarFailure", error)
+      }
+    )
+  },
+  bookSeminarModal({commit}) {
+    commit("bookSeminarModal");
   }
 };
 
@@ -24,6 +44,19 @@ const mutations = {
   },
   getAllFailure(state, error) {
     state.all = { error };
+  },
+  bookSeminarRequest(state) {
+    state.booking = { loading: true, modal: true };
+  },
+  bookSeminarSuccess(state) {
+    state.booking = { success: true, modal: false };
+  },
+  bookSeminarFailure(state, error) {
+    state.booking = { failure: true, error, type: "alert-danger", modal: true };
+  },
+  bookSeminarModal(state) {
+    let newState = !state.booking.modal
+    state.booking = {modal: newState}
   }
 };
 
